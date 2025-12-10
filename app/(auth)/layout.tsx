c1fcb7f50a0816c41308228f8fde571c5680bbd8
Nazarii <nazaralberda7@gmail.com>
2025-12-10 23:22:49 +0000

    feat: implement signup page; enhance layout styling

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className='flex items-center justify-center min-h-screen px-2 mx-auto max-w-7xl'>
+    <div className='flex items-center justify-center min-h-screen px-2 mx-auto max-w-7xl py-2'>
       {children}
     </div>
   );
