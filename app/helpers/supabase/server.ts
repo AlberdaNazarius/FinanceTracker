aad66b14f9ca792882c66f900db71afd87d28f7a
Nazarii <nazaralberda7@gmail.com>
2026-06-22 20:10:12 +0000

    fix: correct environment variable name for Supabase publishable key

--- app/helpers/supabase/server.ts
+++ app/helpers/supabase/server.ts
@@ -2,7 +2,8 @@ import { createServerClient, type CookieOptions } from "@supabase/ssr";
 import type { ReadonlyRequestCookies } from "next/dist/server/web/spec-extension/adapters/request-cookies";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
+
 
 export const createClient = (cookieStore: ReadonlyRequestCookies) => {
   return createServerClient(
